ebc33fcaf1fa578385642f06349649acb8dceae9
Yuieii <yuieii.uwu@gmail.com>
2025-12-22 17:07:06 +0800

    ue: Refactored some mutex logic

---
+++ ue.Core.Tests/MutexTests.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) 2025 Yuieii.
+
+namespace ue.Core.Tests
+{
+    public class MutexTests
+    {
+        [Test]
+        public void TestAcquireShared()
+        {
+            var mutex = new Mutex<int>(1);
+            using var l = mutex.AcquireShared();
+            Assert.That(l.Value, Is.EqualTo(1));
+
+            var thread = new Thread(() =>
+            {
+                var r = mutex.AcquireShared();
+                Assert.That(r.Value, Is.EqualTo(1));
+            });
+            thread.Start();
+            thread.Join();
+        }
+
+        [Test]
+        public void TestAcquireContainer()
+        {
+            var mutex = new Mutex<int>(1);
+
+            using (var l = mutex.AcquireContainer())
+            {
+                l.Value = 2;
+                Assert.That(l.Value, Is.EqualTo(2));
+            }
+            
+            using (var l = mutex.AcquireShared())
+            {
+                Assert.That(l.Value, Is.EqualTo(2));
+            }
+        }
+
+        [Test]
+        public void TestAcquireExclusive()
+        {
+            var mutex = new Mutex<int>(1);
+            using var l = mutex.AcquireContainer();
+            l.Value = 2;
+            
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(l.Value, Is.EqualTo(2));
+                Assert.That(mutex.TryAcquireShared().IsError, Is.True);
+            }
+        }
+        
+        [Test]
+        public void TestTryAcquireExclusive()
+        {
+            var mutex = new Mutex<int>(1);
+            using var l = mutex.AcquireShared();
+            
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(l.Value, Is.EqualTo(1));
+                Assert.That(mutex.TryAcquireExclusive().IsError, Is.True);    
+            }
+        }
+    }
+}
---
+++ ue.Core/MutexGuard.cs
@@ -0,0 +1,89 @@
+﻿// Copyright (c) 2025 Yuieii.
+
+using System;
+using System.Threading;
+
+namespace ue.Core
+{
+    public static class MutexGuardExtensions
+    {
+        extension<T>(IMutexGuard<T> self)
+        {
+            public IMutexGuard<TOut> Select<TOut>(Func<T, TOut> transform)
+                => new MappedMutexGuard<T, TOut>(self, transform);
+        }
+    }
+    
+    public interface IMutexGuard : IDisposable;
+    
+    public interface IMutexGuard<out T> : IMutexGuard
+    {
+        T Value { get; }
+    }
+
+    public interface IContainerMutexGuard<T> : IMutexGuard<T>
+    {
+        public new T Value { get; set; }
+        
+        T IMutexGuard<T>.Value => Value;
+    }
+
+    public interface IUpgradableMutexGuard<out TImmutable, out TMutable> : IMutexGuard<TImmutable>
+    {
+        IMutexGuard<TMutable> Upgrade();
+    }
+    
+    public sealed class MappedMutexGuard<T, TOut> : IMutexGuard<TOut>
+    {
+        private readonly IMutexGuard<T> _inner;
+        private readonly Func<T, TOut> _transform;
+
+        public MappedMutexGuard(IMutexGuard<T> inner, Func<T, TOut> transform)
+        {
+            _inner = inner;
+            _transform = transform;
+        }
+
+        public TOut Value => _transform(_inner.Value);
+
+        public void Dispose() => _inner.Dispose();
+    }
+
+    public sealed class ReadWriteMutexGuard<TMutable> : IMutexGuard<TMutable>
+    {
+        private readonly TMutable _value;
+        private readonly ReaderWriterLockSlim _lock;
+    
+        public ReadWriteMutexGuard(ReaderWriterLockSlim @lock, TMutable value)
+        {
+            _lock = @lock;
+            _value = value;
+        }
+    
+        public TMutable Value => _value;
+
+        public void Dispose()
+        {
+            _lock.ExitWriteLock();
+        }
+    }
+    
+    public sealed class ReadOnlyMutexGuard<TImmutable> : IMutexGuard<TImmutable>
+    {
+        private readonly TImmutable _value;
+        private readonly ReaderWriterLockSlim _lock;
+    
+        public ReadOnlyMutexGuard(ReaderWriterLockSlim @lock, TImmutable value)
+        {
+            _lock = @lock;
+            _value = value;
+        }
+    
+        public TImmutable Value => _value;
+    
+        public void Dispose()
+        {
+            _lock.ExitReadLock();
+        }
+    }
+}
