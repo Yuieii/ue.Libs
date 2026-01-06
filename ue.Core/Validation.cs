de9ea06663cb8b346e1596dcf0889a1b93c48dc1
Yuieii <yuieii.uwu@gmail.com>
2026-01-06 20:19:30 +0800

    ue: Updates

--- ue.Core/Validation.cs
+++ ue.Core/Validation.cs
@@ -17,5 +17,18 @@ namespace ue.Core
             if (value != null) return;
             throw new ArgumentNullException(paramName, "The provided value cannot be null");
         }
+
+        public static void EnsureInclusiveBetween<T>(T start, T end, T value, Func<string> message)
+            where T : IComparable<T>
+        {
+            if (value.CompareTo(start) >= 0 && value.CompareTo(end) <= 0)
+                return;
+            
+            throw new ArgumentException(message());
+        }
+
+        public static void EnsureInclusiveBetween<T>(T start, T end, T value)
+            where T : IComparable<T>
+            => EnsureInclusiveBetween(start, end, value, () => $"The provided value must be between {start} and {end}");
     }
 }
