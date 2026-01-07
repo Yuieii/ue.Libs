2f3b6408efc0aed7be281b44faf8dbfa5df0fbc9
Yuieii <yuieii.uwu@gmail.com>
2026-01-08 03:40:01 +0800

    ue: `Singleton<T>`: Allow singleton to be fetched via `ISingleton<T>`

---
+++ ue.Core.Tests/SingletonTests.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) 2026 Yuieii.
+
+namespace ue.Core.Tests
+{
+    [TestFixture]
+    public class SingletonTests
+    {
+        private class InferredSingleton : ISingleton<InferredSingleton>
+        {
+            public int Value { get; private init; } = 24;
+
+            public static InferredSingleton Instance { get; } = new()
+            {
+                Value = 57
+            };
+        }
+        
+        private class ExplicitSingleton : ISingleton<ExplicitSingleton>
+        {
+            public int Value { get; private init; } = 249;
+
+            static ExplicitSingleton ISingleton<ExplicitSingleton>.Instance { get; } = new()
+            {
+                Value = 573
+            };
+        }
+        
+        [Test]
+        public void TestInferFromInterface()
+        {
+            var inferred = Singleton<InferredSingleton>.Instance;
+            Assert.That(inferred.Value, Is.EqualTo(57));
+            
+            var exp = Singleton<ExplicitSingleton>.Instance;
+            Assert.That(exp.Value, Is.EqualTo(573));
+        }
+    }
+}
--- ue.Core/Singleton.cs
+++ ue.Core/Singleton.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) 2026 Yuieii.
 
 using System;
+using System.Linq;
+using System.Reflection;
 
 namespace ue.Core
 {
@@ -16,13 +18,35 @@ namespace ue.Core
 #if NET7_0_OR_GREATER
         : ISingleton<T>
 #endif
-        where T : class, new()
+        where T : class
     {
-        private static readonly Lazy<T> _instance = new(() => new T());
+        private static readonly Lazy<T> _instance = new(CreateInstance);
         
         /// <summary>
         /// Retrieves the instance of this singleton.
         /// </summary>
         public static T Instance => _instance.Value;
+
+        private static T CreateInstance()
+        {
+#if NET7_0_OR_GREATER
+            if (typeof(T).GetInterfaces().Contains(typeof(ISingleton<T>)))
+            {
+                var properties = typeof(T)
+                    .GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+
+                var property = properties.FirstOrDefault(t => t.Name.EndsWith(".Instance"))
+                    .ToOption()
+                    .OrGet(() => properties.FirstOrDefault(t => t.Name == "Instance").ToOption());
+
+                if (property.IsSome)
+                {
+                    return (T) property.Unwrap().GetValue(null)!;
+                }
+            }
+#endif
+
+            return (T) Activator.CreateInstance(typeof(T))!;
+        }
     }
 }
